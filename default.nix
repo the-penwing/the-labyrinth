c18bec2210f4658e5f1bd53d49375033042963fc
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-28 11:31:33 +1000

    added nix derivations

---
+++ default.nix
@@ -0,0 +1,22 @@
+{
+  python3Packages,
+  lib,
+}:
+python3Packages.buildPythonApplication {
+  pname = "theLabyrinth";
+  version = "0.1.0";
+  src = ./.;
+
+  propagatedBuildInputs = with python3Packages; [
+    textual
+    rich
+    rich-pixels
+    requests
+    pillow
+  ];
+
+  meta = with lib; {
+    description = "Terminal \"Guess the Word\" game in Python + Textual";
+    license = licenses.mit;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "theLabyrinth - Terminal Word Game";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.callPackage ./default.nix {};
+      }
+    );
+}
